d2093818e293b2209efe4f0e3dce302595c3e877
James Dinh <jamesdinh14@gmail.com>
2015-02-23 20:21:39 -0800

    Update MemoWriter.java
    Added comments, replaced "-delim-" with variable: delimiter, fixed to make this closer to Homework requirements, removed unnecessary instance variables

--- MemoWriter.java
+++ MemoWriter.java
@@ -1,3 +1,9 @@
+/* James Dinh
+ * 2015/02/23
+ * Purpose: Write and read memos from a file
+ * Input: File name, Memo topic, Memo body into File
+ * Output: Memo topic, Date Stamp, Memo Body from File
+ */
 import java.io.File;
 import java.util.Scanner;
 import java.io.PrintWriter;
@@ -6,38 +12,49 @@ import java.io.FileNotFoundException;
 public class MemoWriter {
   
   //instance variables
-  private File outFile;
-  private Scanner input;
-  private PrintWriter writer;
   private java.util.Date dateStamp;
+  private PrintWriter writer;
+  private String delimiter;
   
-  MemoWriter(String outFileName) {
+  // Constructor to initialize date stamp and delimiter
+  MemoWriter(String delimiter) {
     dateStamp = new java.util.Date();
-    outFile = new File(outFileName);
-    input = new Scanner(System.in);
+    this.delimiter = delimiter;
+  }
+
+  // Write memos to a File
+  public void writeMemo(String outFileName) {
+    String choice;
+    File outFile = new File(outFileName);
+    Scanner input = new Scanner(System.in);
     if (outFile.exists()) {
       System.out.println("File already exists. Exiting...");
       System.exit(0);
     }
+    
     try {
-    writer = new PrintWriter(outFile);
-    } 
+      writer = new PrintWriter(outFile);
+      
+      // Loop to allow user to enter memos to their heart's content
+      do {
+        System.out.println("Enter the topic of memo: ");
+        writer.print(input.nextLine() + delimiter);
+        writer.print(dateStamp.toString() + delimiter);
+        
+        System.out.println("Enter the line of text for your memo: ");
+        writer.print(input.nextLine() + delimiter);
+        
+        System.out.println("Would you like to enter another memo? (Y/N)");
+        choice = input.nextLine();
+      } while (choice.charAt(0) == 'y' || choice.charAt(0) == 'Y');
+    }
     catch (FileNotFoundException fnfe) {
       System.out.println("File not found.");
     }
-  }
-
-  public void writeMemo(int numOfMemos) {
-    for (int i = 0; i < numOfMemos; i++) {
-      System.out.println("Enter the topic of memo " + (i + 1) + ": ");
-      writer.print(input.nextLine() + "-delim-");
-      writer.print(dateStamp.toString() + "-delim-");
-      
-      System.out.println("Enter the line of text for your memo: ");
-      writer.print(input.nextLine() + "-delim-");
+    finally {
+      // Close PrintWriter and Scanner
+      writer.close();
+      input.close();
     }
-    writer.close();
-    input.close();
   }
-  
 }
