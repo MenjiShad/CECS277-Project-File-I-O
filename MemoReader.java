b1dd0864644916fcd990a411ea1ae41aff361a79
James Dinh <jamesdinh14@gmail.com>
2015-02-23 20:26:59 -0800

    Update MemoReader.java
    Added comments, removed unnecessary instance variables

--- MemoReader.java
+++ MemoReader.java
@@ -1,3 +1,9 @@
+/* James Dinh
+ * 2015/02/23
+ * Purpose: Write and read memos from a file
+ * Input: File name, Memo topic, Memo body into File
+ * Output: Memo topic, Date Stamp, Memo Body from File
+ */
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
@@ -6,47 +12,58 @@ import javax.swing.JFileChooser;
 public class MemoReader {
   
   // instance variables
-  private JFileChooser dialogBox;
-  private File inFile;
   private Scanner fileReader;
   private Scanner input = new Scanner(System.in);
+  private String delimiter;
   
-  MemoReader() {}
+  // Constructor to initialize the delimiter
+  MemoReader(String delimiter) {
+    this.delimiter = delimiter;
+  }
   
+// Read memos from a File
   public void readMemo() {
-    dialogBox = new JFileChooser();
+    JFileChooser dialogBox = new JFileChooser();
     if (dialogBox.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-      inFile = dialogBox.getSelectedFile();
+      File inFile = dialogBox.getSelectedFile();
+      
       try {
-      fileReader = new Scanner(inFile);
-      fileReader.useDelimiter("-delim-");
-      } 
+        fileReader = new Scanner(inFile);
+        fileReader.useDelimiter(delimiter); // set the delimiter
+        
+        // As long as there are memos, enable option to read
+        int counter = 0;
+        while (fileReader.hasNextLine()) {
+          // Each memo is 3 lines, so pause after 3 lines are read
+          if (counter % 3 != 0 || counter == 0) { 
+            System.out.println(fileReader.next());
+            counter++;
+          } else {
+            System.out.println("Read the next memo? (Y/N)");
+            String choice = input.next();
+            counter = 0; // Reset counter
+            
+            // If there are no more memos, end of file is reached
+            if (!fileReader.hasNext()) {
+              System.out.println("End of file reached.");
+              break;
+            }
+            
+            // If the user does not want to read memos, exit the loop
+            // Otherwise, read another memo
+            if (choice.charAt(0) == 'N' || choice.charAt(0) == 'n')
+              break;
+          }
+        }
+      }
       catch (FileNotFoundException fnfe) {
         System.out.println("File not found.");
       }
-      
-      int counter = 0;
-      while (fileReader.hasNextLine()) {
-        if (counter % 3 != 0 || counter == 0) {
-          System.out.println(fileReader.next());
-          counter++;
-        } else {
-          System.out.println("Read the next memo? (Y/N)");
-          String choice = input.next();
-          counter = 0;
-          
-          if (!fileReader.hasNext()) {
-            System.out.println("End of File reached.");
-            break;
-          }
-          
-          if (choice.charAt(0) == 'N' || choice.charAt(0) == 'n')
-            break;
-        }
+      finally {
+        // Close Scanners
+        fileReader.close();
+        input.close();
       }
-    }
-    fileReader.close();
-    input.close();
+    } 
   }
-  
 }
